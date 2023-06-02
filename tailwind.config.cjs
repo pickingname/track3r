3c9fd44fa6f21410e4c24dfdd6007bdba7ff6fbd
pickingname <trintrinir@icloud.com>
2023-06-02 19:49:31 +0700

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html, css, js}","./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        // add more fonts ...name: [...source-of-font]
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1024px",
+          xl: "1024px",
+          "2xl": "1024px",
+        },
+      },
+      colors: {
+        'badass-black': '#111111',
+        'verydark-blue' : '#060b36',
+      },
+    },
+  },
+  plugins: [
+    // add your tailwind plugins
+    require('flowbite/plugin'),
+  ],
+};
