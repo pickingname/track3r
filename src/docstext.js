b2179f5d18852b4bd977a71009accb65f21b98f0
pickingname <trintrinir@icloud.com>
2023-06-07 20:26:18 +0700

    added the disclaimber

--- src/docstext.js
+++ src/docstext.js
@@ -2,6 +2,8 @@
 // it will search and display you a docs
 // it will be implemented after theres a docs[]!
 
+console.log('infinite loop incoming!! it will be changed after the docs features!!!11')
+
 
 setInterval(() => {
     var element = document.getElementById("docstext");
