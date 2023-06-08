0d3802aa7079ffc4946bb78224e20f1937f301f4
pickingname <trintrinir@icloud.com>
2023-06-08 18:58:29 +0700

    error fix real

--- src/time.js
+++ src/time.js
@@ -1,23 +1,23 @@
-  fetch('https://api.github.com/repos/pickingname/track3r/commits?per_page=5')
-  console.debug('fetching started: https://api.github.com/repos/pickingname/track3r/commits?per_page=5')
-    .then(response => response.json())
-    .then(data => {
-      // Iterate over the commits and append commit message, author, and time
-      data.forEach((commit, index) => {
-        const commitElement = document.getElementById(`time-${index + 1}`);
-        if (commitElement) {
-          const authorName = commit.commit.author.name;
-          const commitMessage = commit.commit.message;
-          const commitTime = new Date(commit.commit.author.date).toLocaleString('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            year: '2-digit'
-          });
-          commitElement.innerHTML = `Issued by: ${authorName}<br>Time: ${commitTime}`;
-        }
-      });
-    })
-    .catch(error => console.log(error));
+console.debug('fetching started: https://api.github.com/repos/pickingname/track3r/commits?per_page=5');
+fetch('https://api.github.com/repos/pickingname/track3r/commits?per_page=5')
+  .then(response => response.json())
+  .then(data => {
+    // Iterate over the commits and append commit message, author, and time
+    data.forEach((commit, index) => {
+      const commitElement = document.getElementById(`time-${index + 1}`);
+      if (commitElement) {
+        const authorName = commit.commit.author.name;
+        const commitMessage = commit.commit.message;
+        const commitTime = new Date(commit.commit.author.date).toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          year: '2-digit'
+        });
+        commitElement.innerHTML = `Issued by: ${authorName}<br>Time: ${commitTime}`;
+      }
+    });
+  })
+  .catch(error => console.log(error));
