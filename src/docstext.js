b516e1882cf17592ed07205a7def2595432b9646
pickingname <trintrinir@icloud.com>
2023-06-08 05:10:02 +0000

    mock placeholder (it will change when there are contects)

--- src/docstext.js
+++ src/docstext.js
@@ -3,23 +3,8 @@
 // it will be implemented after theres a docs[]!
 
 console.log('infinite loop incoming!! it will be changed after the docs features!!!11')
-
-
-setInterval(() => {
-    var element = document.getElementById("docstext");
-
-    // Set the element's text to "New text"
-    element.textContent = "There are currently no archive availble.";
-    setInterval(() => {
-        var element = document.getElementById("docstext");
-    
-        // Set the element's text to "New text"
-        element.textContent = "There are currently no archive availble..";
-        setInterval(() => {
-            var element = document.getElementById("docstext");
-        
-            // Set the element's text to "New text"
-            element.textContent = "There are currently no archive availble...";
-          }, 1000);
-      }, 1000);
-  }, 1000);
+var element = document.getElementById("docstext");
+var statuscode = document.getElementById("statuscode")
+// Set the element's text to "New text"
+element.textContent = "There are currently no archive availble.";
+statuscode.textContent = '204, No Content';
